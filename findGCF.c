2ad5640d3a5602f48e5c31ac917aa4c3c36facc5
bojoluis21798 <bojoluis21798@gmail.com>
2021-02-25 23:29:10 +0800

    Create find gcf

---
+++ findGCF.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int findGCF(int x, int y) {
+	int i, max = 1;
+
+	for(i = 1; i <= x && i < y; i++) {
+		if (x % i == 0 && y % i == 0 && i > max) {
+			max = i;
+		}
+	}
+
+	return max;
+}
+
+int main () {
+	int x, y;
+	int result;
+
+	scanf("%d %d", &x, &y);
+	result = findGCF(x, y);
+
+	printf("GCF: %d", result);
+	return 0;
+}
