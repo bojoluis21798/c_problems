22cfee51f507f4125cde3a220423d33b9b197e7e
bojoluis21798 <bojoluis21798@gmail.com>
2021-02-25 23:46:17 +0800

    Fix for condition

--- findGCF.c
+++ findGCF.c
@@ -3,7 +3,7 @@
 int findGCF(int x, int y) {
 	int i, max = 1;
 
-	for(i = 1; i <= x && i < y; i++) {
+	for(i = 1; i <= x && i <= y; i++) {
 		if (x % i == 0 && y % i == 0 && i > max) {
 			max = i;
 		}
